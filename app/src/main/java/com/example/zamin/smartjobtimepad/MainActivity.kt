ec8e7837b6167ea6389806b7752acbe88262e085
SardorSafarov <zomin000s@gmail.com>
2023-01-13 10:15:20 +0500

    begin 1

--- app/src/main/java/com/example/zamin/smartjobtimepad/MainActivity.kt
+++ app/src/main/java/com/example/zamin/smartjobtimepad/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.zamin.smartjobtimepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.zamin.smartjobtimepad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Bismillahir rohmanir rohim!!!
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/example/zamin/smartjobtimepad/app/app.kt
@@ -0,0 +1,9 @@
+package com.example.zamin.smartjobtimepad.app
+
+import android.util.Log
+
+
+fun D(message:String)
+{
+    Log.d("sardor","->>>>>>    $message    <<<<<<-")
+}
