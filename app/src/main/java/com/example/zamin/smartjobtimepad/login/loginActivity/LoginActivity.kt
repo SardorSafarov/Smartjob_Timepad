a48083df23c6d2e0f92caeddc0aa6dc5f2c7785f
SardorSafarov <zomin000s@gmail.com>
2023-01-15 10:19:18 +0500

    begin 4

--- app/src/main/java/com/example/zamin/smartjobtimepad/login/loginActivity/LoginActivity.kt
+++ app/src/main/java/com/example/zamin/smartjobtimepad/login/loginActivity/LoginActivity.kt
@@ -1,8 +1,17 @@
 package com.example.zamin.smartjobtimepad.login.loginActivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
+import android.text.TextPaint
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.zamin.smartjobtimepad.databinding.ActivityLoginBinding
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
@@ -10,6 +19,17 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
+        binding.apply {
+            val paint = txt.paint
+            val width = paint.measureText(txt.text.toString())
+            val textShader: Shader = LinearGradient(0f, 0f, width, txt.textSize, intArrayOf(
+                Color.parseColor("#3f77f5"),
+                Color.parseColor("#4ba8fc")
+            ), null, Shader.TileMode.REPEAT)
+
+            txt.paint.setShader(textShader)
+        }
+
     }
 }
