9448c1ef270e8da0092aceb58dabda76d48ccd60
SardorSafarov <zomin000s@gmail.com>
2023-01-16 10:53:10 +0500

    qr code 1

---
+++ app/src/main/java/com/example/zamin/smartjobtimepad/login/fragments/QrCodeScanerFragment.kt
@@ -0,0 +1,17 @@
+package com.example.zamin.smartjobtimepad.login.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.zamin.smartjobtimepad.BaseFragment
+import com.example.zamin.smartjobtimepad.R
+import com.example.zamin.smartjobtimepad.databinding.FragmentQrCodeScanerBinding
+
+class QrCodeScanerFragment : BaseFragment<FragmentQrCodeScanerBinding>(FragmentQrCodeScanerBinding::inflate) {
+    override fun onViewCreate() {
+
+    }
+
+}
