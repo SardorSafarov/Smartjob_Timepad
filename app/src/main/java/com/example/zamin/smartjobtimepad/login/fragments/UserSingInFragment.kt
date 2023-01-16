b9f7649f2fc15a578295a39955c4bf38aa3147cb
SardorSafarov <zomin000s@gmail.com>
2023-01-16 22:39:23 +0500

    alert dialog

--- app/src/main/java/com/example/zamin/smartjobtimepad/login/fragments/UserSingInFragment.kt
+++ app/src/main/java/com/example/zamin/smartjobtimepad/login/fragments/UserSingInFragment.kt
@@ -1,19 +1,20 @@
-package com.example.zamin.smartjobtimepad.login.fragments
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.zamin.smartjobtimepad.BaseFragment
-import com.example.zamin.smartjobtimepad.R
 import com.example.zamin.smartjobtimepad.app.setGradiandColor
 import com.example.zamin.smartjobtimepad.databinding.FragmentUserSingInBinding
+
 
 class UserSingInFragment : BaseFragment<FragmentUserSingInBinding>(FragmentUserSingInBinding::inflate) {
     override fun onViewCreate() {
     binding.txt.setGradiandColor()
     binding.txt1.setGradiandColor()
+        setOnClick()
+    }
+
+    private fun setOnClick() {
+        binding.apply {
+
+        }
     }
 
 }
