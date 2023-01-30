03ffb0a874a75886f0efca0984b7c130b210d4bd
SardorSafarov <zomin000s@gmail.com>
2023-01-30 22:16:12 +0500

    language select done

--- app/src/main/java/com/example/zamin/smartjobtimepad/login/fragments/auth/signIn/SignInFragment.kt
+++ app/src/main/java/com/example/zamin/smartjobtimepad/login/fragments/auth/signIn/SignInFragment.kt
@@ -2,8 +2,13 @@ package com.example.zamin.smartjobtimepad.login.fragments.auth.signIn
 
 
 
-import com.example.zamin.smartjobtimepad.login.fragments.user.signIn.UserSingInFragment1
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.appcompat.view.menu.MenuPopupHelper
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.zamin.smartjobtimepad.BaseFragment
@@ -13,16 +18,24 @@ import com.example.zamin.smartjobtimepad.app.setGradiandColor
 import com.example.zamin.smartjobtimepad.databinding.FragmentSignInBinding
 import com.example.zamin.smartjobtimepad.login.fragments.auth.signUp.SignUpFragment
 import com.example.zamin.smartjobtimepad.login.fragments.auth.signUp.TelNumberFragment
+import com.example.zamin.smartjobtimepad.login.fragments.user.signIn.UserSingInFragment1
 
 
-class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding::inflate) {
+class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding::inflate){
+//    var countryNames = arrayOf("O’zbekcha", "Русский", "English")
+//    var flags = intArrayOf(
+//        R.drawable.flag_uzbekistan,
+//        R.drawable.flag_ru,
+//        R.drawable.flag_en,
+//    )
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onViewCreate() {
         chackCameraPerimition()
         setClick()
         binding.txt.setGradiandColor()
-
     }
+
     private fun setClick() {
         binding.apply {
             btnTelCode.setOnClickListener {
@@ -44,10 +57,14 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding
             btnHelp.setOnClickListener {
                 dialogHelp(requireContext())
             }
+            btnLanguage.setOnClickListener{
+                showPopup(it)
+            }
         }
 
 
     }
+
     private fun chackCameraPerimition() {
 
         if (ContextCompat.checkSelfPermission(
@@ -64,4 +81,43 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding
 
     }
 
+    @SuppressLint("RestrictedApi")
+    private fun showPopup(view: View) {
+
+        val menuBuilder = MenuBuilder(requireContext())
+        val inflater = MenuInflater(requireContext())
+        inflater.inflate(R.menu.item_language_kod, menuBuilder)
+        val optionsMenu = MenuPopupHelper(requireContext(), menuBuilder, view)
+        optionsMenu.setForceShowIcon(true)
+        menuBuilder.setCallback(object : MenuBuilder.Callback {
+            override fun onMenuItemSelected(menu: MenuBuilder, item: MenuItem): Boolean {
+                when(item.itemId)
+                {
+                    R.id.language_uz->{
+                        binding.apply {
+                            languageImage.setImageResource(R.drawable.flag_uzbekistan)
+                            languageText.text = "O’zbekcha"
+                        }
+                    }
+                    R.id.language_ru->{
+                        binding.apply {
+                            languageImage.setImageResource(R.drawable.flag_ru)
+                            languageText.text = "Русский"
+                        }
+                    }
+                    R.id.language_en->{
+                        binding.apply {
+                            languageImage.setImageResource(R.drawable.flag_en)
+                            languageText.text = "English"
+                        }
+                    }
+                    else -> false
+                }
+                return true
+            }
+
+            override fun onMenuModeChange(menu: MenuBuilder) {}
+        })
+        optionsMenu.show()
+    }
 }
---
+++ app/src/main/java/com/example/zamin/smartjobtimepad/models/ModelLanguage.kt
@@ -0,0 +1,7 @@
+package com.example.zamin.smartjobtimepad.models
+
+data class ModelLanguage (
+    var id:Int,
+    var image:Int,
+    var name:String,
+            )
